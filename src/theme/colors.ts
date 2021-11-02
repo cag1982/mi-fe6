63faa0fb73c7ab149b23095a5088a15a224c1fda
SandmanTeam <info@sandman.finance>
2021-11-01 21:24:02 -0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,55 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#9e3760",
+  primary: "#2b487d",
+  primaryBright: "#627D98",
+  primaryDark: "#334E68",
+  secondary: "#E4C2AB",
+  success: "#7fa94a",
+  warning: "#A6391C",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#b3e0e6",
+  backgroundDisabled: "#aaa",
+  contrast: "rgba(224, 224, 224, .12)",
+  invertedContrast: "#FFFFFF",
+  input: "rgba(224, 224, 224, .12)",
+  tertiary: "rgba(224, 224, 224, .08)",
+  text: "rgba(33, 33, 33, .87)",
+  textDisabled: "rgba(33, 33, 33, .38)",
+  textSubtle: "rgba(33, 33, 33, .60)",
+  borderColor: "rgba(33, 33, 33, .12)",
+  card: "rgba(224, 224, 224, .5)",
+  modal: "#323232",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #272727 0%, #353535 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#b3e0e6",
+  backgroundDisabled: "#aaa",
+  contrast: "rgba(224, 224, 224, .12)",
+  invertedContrast: "#FFFFFF",
+  input: "rgba(224, 224, 224, .12)",
+  tertiary: "rgba(224, 224, 224, .08)",
+  text: "rgba(255, 255, 255, .87)",
+  textDisabled: "rgba(255, 255, 255, .38)",
+  textSubtle: "rgba(255, 255, 255, .60)",
+  borderColor: "rgba(224, 224, 224, .12)",
+  card: "rgba(224, 224, 224, .5)",
+  modal: "#323232",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #272727 0%, #353535 100%)",
+  },
+};
